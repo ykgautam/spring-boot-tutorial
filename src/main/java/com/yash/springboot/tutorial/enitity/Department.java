8871845f27f1eaf46436a8cb151f2f321a7fb2e3
Yash <yk.gautam97@gmail.com>
2023-04-22 11:55:18 +0530

    - Add Hibernate validation

--- src/main/java/com/yash/springboot/tutorial/enitity/Department.java
+++ src/main/java/com/yash/springboot/tutorial/enitity/Department.java
@@ -1,9 +1,12 @@
 package com.yash.springboot.tutorial.enitity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 
 @Entity
 public class Department {
@@ -12,6 +15,19 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
 
+    @NotBlank(message = "please add department name")
+    /* other examples of annotations
+    @Length(max = 10, min = 1)
+    @Size(max = 15, min = 1)
+    @Positive
+    @Negative
+    @Email
+    @PositiveOrZero
+    @NegativeOrZero
+    @Future // for date fiels
+    @Past
+    @PastOrPresent
+    @FutureOrPresent*/
     private String departmentName;
 
     private String departmentAddress;
